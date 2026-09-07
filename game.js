8e5ddba84640406c03f56c5faba49acfc27699dc
Jhonatan Torres <jhonatan.menesest@gmail.com>
2026-09-07 13:32:46 -0500

    Eliminar el movimiento

--- game.js
+++ game.js
@@ -9,15 +9,6 @@ const H = 600;
 const keys = {};
 const justPressed = {};
 
-window.addEventListener('keydown', (e) => {
-  if (!keys[e.code]) justPressed[e.code] = true;
-  keys[e.code] = true;
-});
-
-window.addEventListener('keyup', (e) => {
-  keys[e.code] = false;
-});
-
 function pressed(code) {
   const val = justPressed[code];
   justPressed[code] = false;
